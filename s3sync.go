68cea0af940475bae05e2aaafb16995429f8fade
knorrkator <knorrkator@posteo.de>
2015-12-08 19:44:03 +0100

    Basic algorithm done of how to determine difference between Source- and Destination-Bucket

--- s3sync.go
+++ s3sync.go
@@ -47,7 +47,77 @@ func list_elements(svc *s3.S3, bucket_name string, result_chan chan *s3.ListObje
     return
   }
 
-  fmt.Printf("%s done\n", bucket_name)
+  //fmt.Printf("%s done\n", bucket_name)
+}
+
+// Need to convert Chungs of Keys, returned from S3-API into lists
+func extract_contents(result_chan chan *s3.ListObjectsOutput, output_chan chan *s3.Object) {
+  defer wg.Done()
+  for {
+    s3_list_chunk, ok := <- result_chan
+    if !ok {
+      // Channel is closed bei producer
+      break
+    }
+
+    for _, s3_key := range s3_list_chunk.Contents {
+      //fmt.Printf("Key: %v\n", s3_key)
+      output_chan <- s3_key
+    }
+
+    close(output_chan)
+
+  }
+}
+
+// Detects difference between Source- and Destination-Bucket
+func find_missing(s3_contents_src, s3_contents_dest chan *s3.Object) {
+  defer wg.Done()
+  var src, dest *s3.Object
+
+  src = <- s3_contents_src
+  dest = <- s3_contents_dest
+
+  for {
+
+    if src == nil && dest == nil {
+      // Case A: no elements left in both Buckets
+      break
+
+    } else if src != nil && dest != nil {
+      // Case B: got an element in both Buckets
+
+      if *src.Key < *dest.Key {
+        // src-element is ahead of destination-element. Src-element is missing in Destination-Bucket
+        src = <- s3_contents_src
+
+      } else if *src.Key > *dest.Key {
+        // src-element is beyond destination-element. Destination-element exists only on Destination-Bucket
+        dest = <- s3_contents_dest
+
+      } else {  // src.Key == dest.Key
+        // Src and Dest are equal
+        src = <- s3_contents_src
+        dest = <- s3_contents_dest
+
+      }
+
+    } else if dest == nil {
+      // Case C: no element left in Destination-Bucket but in Source-Bucket
+      src = <- s3_contents_src
+
+    } else if src == nil {
+      // Case D: no elements left in Source-Bucket but in Destination-Bucket
+      dest = <- s3_contents_dest
+
+    } else {
+      // This means not Case A or not Case B or not Case C or not Case D
+      fmt.Printf("This case shouldn't appear. However it did. Src: %v Dest: %v", src, dest)
+      // TODO: raise an error. Stop execution
+      break
+
+    }
+  }
 }
 
 func main() {
@@ -56,13 +126,18 @@ func main() {
     // exporting the AWS_REGION environment variable
     svc := s3.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
 
-    wg.Add(2)
+    wg.Add(5)
 
     result_chan_src := make(chan *s3.ListObjectsOutput, CHAN_BUFFER_SIZE)
     result_chan_dest := make(chan *s3.ListObjectsOutput, CHAN_BUFFER_SIZE)
+    s3_contents_src := make(chan *s3.Object, CHAN_BUFFER_SIZE)
+    s3_contents_dest := make(chan *s3.Object, CHAN_BUFFER_SIZE)
 
     go list_elements(svc, "knorrtestx1", result_chan_src)
     go list_elements(svc, "knorrtestx2", result_chan_dest)
+    go extract_contents(result_chan_src, s3_contents_src)
+    go extract_contents(result_chan_dest, s3_contents_dest)
+    go find_missing(s3_contents_src, s3_contents_dest)
 
     wg.Wait()
 
