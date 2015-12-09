c1c74a7d560581322c1a6b69a283938c6357aa48
knorrkator <knorrkator@posteo.de>
2015-12-09 22:59:45 +0100

    S3-Keys that are missing in Destination-Bucket are now syncted to it from Source-Bucket

--- s3sync.go
+++ s3sync.go
@@ -16,9 +16,12 @@ const (
   S3_MAX_KEYS = 1000
   // How many S3-Result-Sets are buffered until the producer blocks
   CHAN_BUFFER_SIZE = 1000
+  // How many worker processes should do actual sync from Source-Bucket to Destination-Bucket
+  CHAN_UPLOAD_WORKER = 5
 )
 
-var wg sync.WaitGroup
+var wg_list sync.WaitGroup
+var wg_upload sync.WaitGroup
 
 // Finds every element in a S3-Bucket
 // Is meant to run in parallel. Puts resultsets into channel *s3.ListObjectsOutput
@@ -27,7 +30,7 @@ var wg sync.WaitGroup
 // Second is the S3-Bucket-Name (Note not the S3-Path like s3://bucket_name)
 // Third is the result channel of type *s3.ListObjectsOutput
 func receive_s3_chunks(svc *s3.S3, bucket_name string, chan_s3_chunks chan *s3.ListObjectsOutput) {
-  defer wg.Done()
+  defer wg_list.Done()
   defer close(chan_s3_chunks)
 
   params := &s3.ListObjectsInput{
@@ -52,7 +55,7 @@ func receive_s3_chunks(svc *s3.S3, bucket_name string, chan_s3_chunks chan *s3.L
 
 // Need to convert Chungs of Keys, returned from S3-API into lists
 func extract_contents(chan_s3_chunks chan *s3.ListObjectsOutput, output_chan chan *s3.Object) {
-  defer wg.Done()
+  defer wg_list.Done()
   for {
     s3_list_chunk, ok := <- chan_s3_chunks
     if !ok {
@@ -71,8 +74,14 @@ func extract_contents(chan_s3_chunks chan *s3.ListObjectsOutput, output_chan cha
 }
 
 // Detects difference between Source- and Destination-Bucket
-func calculate_diff(s3_contents_src, s3_contents_dest chan *s3.Object) {
-  defer wg.Done()
+// s3_contents_src: Input-Channel. Contains every element in Source-Bucket.
+// s3_contents_dest: Input-Channel. Contains every element in Destination-Bucket
+// s3_contents_4_upload: Output-Channel. Receives every Elemente from Source-Bucket
+//                       that does not exist in Destination-Bucket. When done this
+//                       Channel is closed.
+func calculate_diff(s3_contents_src, s3_contents_dest, s3_contents_4_upload chan *s3.Object) {
+  defer wg_list.Done()
+  defer close(s3_contents_4_upload)
   var src, dest *s3.Object
 
   src = <- s3_contents_src
@@ -89,25 +98,38 @@ func calculate_diff(s3_contents_src, s3_contents_dest chan *s3.Object) {
 
       if *src.Key < *dest.Key {
         // src-element is ahead of destination-element. Src-element is missing in Destination-Bucket
+        fmt.Printf("Adding %v for upload\n", *src.Key)
+        s3_contents_4_upload <- src
+        // Continue with next element
         src = <- s3_contents_src
 
       } else if *src.Key > *dest.Key {
         // src-element is beyond destination-element. Destination-element exists only on Destination-Bucket
+
+        // Continue with next element
         dest = <- s3_contents_dest
 
       } else {  // src.Key == dest.Key
         // Src and Dest are equal
+        // Continue with next element
         src = <- s3_contents_src
+        // Continue with next element
         dest = <- s3_contents_dest
 
       }
 
     } else if dest == nil {
       // Case C: no element left in Destination-Bucket but in Source-Bucket
+      // Continue with next element
+
+      fmt.Printf("Adding %v for upload\n", *src.Key)
+      s3_contents_4_upload <- src
+
       src = <- s3_contents_src
 
     } else if src == nil {
       // Case D: no elements left in Source-Bucket but in Destination-Bucket
+      // Continue with next element
       dest = <- s3_contents_dest
 
     } else {
@@ -120,32 +142,86 @@ func calculate_diff(s3_contents_src, s3_contents_dest chan *s3.Object) {
   }
 }
 
+// Syncronizes elements received from Channel s3_contents_4_upload with given Destination-Bucket
+// This function is able to run in parralel with same Channel
+func sync_s3_elements(svc *s3.S3, bucket_name_src, bucket_name_dest string, s3_contents_4_upload chan *s3.Object, worker_id int) {
+  defer wg_upload.Done()
+  var elem *s3.Object
+  var ok bool
+
+  for {
+    elem, ok = <- s3_contents_4_upload
+
+    if ok && elem != nil {
+
+      fmt.Printf("Sync from Worker %v: %v\n", worker_id, *elem.Key)
+      params := &s3.CopyObjectInput{
+      	Bucket:                         aws.String(bucket_name_dest), // Required
+      	CopySource:                     aws.String(bucket_name_src + "/" +  *elem.Key),  // Required
+      	Key:                            aws.String(*elem.Key),        // Required
+      }
+      _, err := svc.CopyObject(params)
+
+      if err != nil {
+      	// Print the error, cast err to awserr.Error to get the Code and
+      	// Message from an error.
+      	fmt.Println(err.Error())
+      	return
+      }
+
+      //fmt.Println(resp)
+
+    } else {
+      break
+    }
+  }
+  fmt.Printf("Sync done\n")
+}
+
 func main() {
     // Create an S3 service object in the "eu-west-1" region
     // Note that you can also configure your region globally by
     // exporting the AWS_REGION environment variable
     svc := s3.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
 
-    wg.Add(5)
-
+    // Channel that contains a list of S3-Chunks
     chan_s3_chunks_src := make(chan *s3.ListObjectsOutput, CHAN_BUFFER_SIZE)
+    // Channel that contains a list of S3-Chunks
     chan_s3_chunks_dest := make(chan *s3.ListObjectsOutput, CHAN_BUFFER_SIZE)
+    // List that contains a list of S3-Keys
     s3_contents_src := make(chan *s3.Object, CHAN_BUFFER_SIZE)
+    // List that contains a list of S3-Keys
     s3_contents_dest := make(chan *s3.Object, CHAN_BUFFER_SIZE)
+    // List that contains a list of S3-Keys that are missing in Destination => need upload to Destination
+    s3_contents_4_upload := make(chan *s3.Object, CHAN_UPLOAD_WORKER)
 
     // Task 1: Read Chunks from S3-Buckets.
     // Chunks are a bunch of S3-Keys that the Bucket contains. A S3-Bucket retuns
     // a maximum of 1.000 keys per Chunk. Both Buckets are read asyncroneuous.
     go receive_s3_chunks(svc, "knorrtestx1", chan_s3_chunks_src)
+    wg_list.Add(1)
     go receive_s3_chunks(svc, "knorrtestx2", chan_s3_chunks_dest)
+    wg_list.Add(1)
     // When the first Chunks arrive we extract their keys and write them into a list.
     // Both Buckets have their own list of S3-Keys
     go extract_contents(chan_s3_chunks_src, s3_contents_src)
+    wg_list.Add(1)
     go extract_contents(chan_s3_chunks_dest, s3_contents_dest)
+    wg_list.Add(1)
     // When the first elements arrive in the list of S3-Keys we calculate the difference
     // between both Bucket. This job starts as soon as there are elements in one of the lists
-    go calculate_diff(s3_contents_src, s3_contents_dest)
+    go calculate_diff(s3_contents_src, s3_contents_dest, s3_contents_4_upload)
+    wg_list.Add(1)
 
-    wg.Wait()
+    // Upload files that are missing on Destination-Bucket
+    fmt.Printf("Worker-Count: %v\n", CHAN_UPLOAD_WORKER)
+    for i := 0; i < CHAN_UPLOAD_WORKER; i++ {
+      fmt.Printf("Starting Worker %v\n", i)
+      wg_upload.Add(1)
+      go sync_s3_elements(svc, "knorrtestx1", "knorrtestx2", s3_contents_4_upload, i)
+    }
+
+    wg_list.Wait()
+    wg_upload.Wait()
 
 }
